2ecdaac31636c6cccf27840cfe2a3f3b6a44ea83
Kyrenia <77701345+Kyreniah@users.noreply.github.com>
2023-06-07 20:37:44 +0200

    Solution to the third stage improved
    Replaced win condition with a method and removed unneeded and commented lines

--- Main.java
+++ Main.java
@@ -6,62 +6,18 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
-        System.out.println("---------");
-
-        /*System.out.printf("| %c %c %c |%n",input.charAt(0),input.charAt(1),input.charAt(2));
-        System.out.printf("| %c %c %c |%n",input.charAt(3),input.charAt(4),input.charAt(5));
-        System.out.printf("| %c %c %c |%n",input.charAt(6),input.charAt(7),input.charAt(8));*/
-
-        /*char[] line1 = {input.charAt(0),input.charAt(1),input.charAt(2)};
-        char[] line2 = {input.charAt(3),input.charAt(4),input.charAt(5)};
-        char[] line3 = {input.charAt(6),input.charAt(7),input.charAt(8)};*
-
-        String[] lines = {line1,line2,line3};*/
         char[] chars = input.toCharArray();
+
+        System.out.println("---------");
         for (int i = 0; i < input.length(); i += 3) {
             System.out.printf("| %c %c %c |%n", chars[i], chars[i + 1], chars[i + 2]);
         }
-
         System.out.println("---------");
-        boolean xWins = false;
-        boolean oWins = false;
-        //X wins horizontal
-        for (int i = 0; i < input.length(); i += 3) {
-            if (chars[i] == 'X' && chars[i + 1] == 'X' && chars[i + 2] == 'X') {
-                xWins = true;
-                break;
-            }
-        }
-        //X wins vertical
-        for (int i = 0; i < 3; i++) {
-            if (chars[i] == 'X' && chars[i+3] == 'X' && chars[i+6] == 'X') {
-                xWins = true;
-                break;
-            }
-        }
-        //X wins diagonal
-        if (chars[0] == 'X' && chars[4] == 'X' && chars[8] == 'X' || chars[2] == 'X' && chars[4] == 'X' && chars[6] == 'X') {
-            xWins = true;
-        }
-        //O wins horizontal
-        for (int j = 0; j < input.length(); j += 3) {
-            if (chars[j] == 'O' && chars[j + 1] == 'O' && chars[j + 2] == 'O') {
-                oWins = true;
-                break;
-            }
-        }
-        //O wins vertical
-        for (int i = 0; i < 3; i++) {
-            if (chars[i] == 'O' && chars[i+3] == 'O' && chars[i+6] == 'O') {
-                oWins = true;
-                break;
-            }
-        }
-        //O wins diagonal
-        if (chars[0] == 'O' && chars[4] == 'O' && chars[8] == 'O' || chars[2] == 'O' && chars[4] == 'O' && chars[6] == 'O') {
-            xWins = true;
-        }
-        //difference between number of Xs and Os
+
+        boolean xWins = hasWinningCondition(chars, 'X');
+        boolean oWins = hasWinningCondition(chars, 'O');
+
+        //Difference between number of Xs and Os
         int numberOfX = 0;
         int numberOfO = 0;
         for (int i = 0; i < input.length(); i++) {
@@ -84,5 +40,28 @@ public class Main {
         } else {
             System.out.println("Draw");
         }
+
+    }
+    public static boolean hasWinningCondition(char[] game, char c) {
+        boolean playerWins = false;
+        //Player wins horizontal
+        for (int i = 0; i < game.length; i += 3) {
+            if (game[i] == c && game[i + 1] == c && game[i + 2] == c) {
+                playerWins = true;
+                break;
+            }
+        }
+        //Player wins vertical
+        for (int i = 0; i < 3; i++) {
+            if (game[i] == c && game[i + 3] == c && game[i + 6] == c) {
+                playerWins = true;
+                break;
+            }
+        }
+        //Player wins diagonal
+        if (game[0] == c && game[4] == c && game[8] == c || game[2] == c && game[4] == c && game[6] == c) {
+            playerWins = true;
+        }
+        return playerWins;
     }
 }
